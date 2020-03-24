d1a0a9d59455d4f3f3be54ab65979c2b855857a0
Igor Ináš <igor.inas@toptal.com>
2020-03-24 12:31:26 +0100

    final result

--- src/components/data-source.tsx
+++ src/components/data-source.tsx
@@ -9,12 +9,18 @@ export interface Product {
 
 export type AttributeId = keyof Product;
 
-const products: Product[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => ({
-    id: id,
-    name: 'Name ' + Math.floor(seededRandom() * 10000),
-    description: 'Description ' + Math.floor(seededRandom() * 10000),
-    price: seededRandom() * 10000,
-}))
+const products: Product[] = [
+    {id: 1001, name: 'Car', description: 'Toyota', price: 20000},
+    {id: 1002, name: 'Plane', description: 'Lockheed Martin', price: 3800000},
+    {id: 1003, name: 'Car', description: 'Tesla', price: 92500},
+    {id: 1004, name: 'Aircraft', description: 'Falcon 9', price: 61000000},
+    {id: 1005, name: 'Car', description: 'Renault', price: 15000},
+    {id: 1006, name: 'Aircraft', description: 'Boeing', price: 4300000},
+    {id: 1007, name: 'Aircraft', description: 'Airbus', price: 3500000},
+    {id: 1008, name: 'Rocket', description: 'Soyuz', price: 78000000},
+    {id: 1009, name: 'Car', description: 'BMW', price: 38000},
+    {id: 1010, name: 'Rocket', description: 'Atlas 5', price: 120000000},
+]
 
 function createSortFunction(sortByAttributeId: AttributeId): (a: Product, b: Product) => number {
     switch (sortByAttributeId) {
